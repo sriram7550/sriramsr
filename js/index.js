714e31268185fdbd486bb352ee27c6abe358e9cb
sriram7550 <100337067+sriram7550@users.noreply.github.com>
2023-01-26 21:43:33 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ AOS.init({
     offset: 500, duration: 3000
   })
 
-
-  document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
   
